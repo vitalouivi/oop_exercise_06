f2d7e1b45275306d9b53899d2a46d9f14a12bd05
vitalouivi <45311390+vitalouivi@users.noreply.github.com>
2020-01-22 22:57:33 +0300

    Add files via upload

--- containers/queue.h
+++ containers/queue.h
@@ -18,38 +18,55 @@ namespace containers {
         class forward_iterator {
         public:
             using value_type = T;
-            using reference = T&;
-            using pointer = T*;
+            using reference = T &;
+            using pointer = T *;
             using difference_type = std::ptrdiff_t;
             using iterator_category = std::forward_iterator_tag;
-            explicit forward_iterator(element* ptr);
-            T& operator*();
-            forward_iterator& operator++();
+
+            explicit forward_iterator(element *ptr);
+
+            T &operator*();
+
+            forward_iterator &operator++();
+
             forward_iterator operator++(int);
-            bool operator== (const forward_iterator& other) const;
-            bool operator!= (const forward_iterator& other) const;
+
+            bool operator==(const forward_iterator &other) const;
+
+            bool operator!=(const forward_iterator &other) const;
+
         private:
-            element* it_ptr;
+            element *it_ptr;
             friend queue;
         };
 
         forward_iterator begin();
+
         forward_iterator end();
-        void push(const T& value);
-        T& top();
+
+        void push(const T &value);
+
+        T &top();
+
         void pop();
+
         void delete_by_it(forward_iterator d_it);
+
         void delete_by_number(size_t N);
-        void insert_by_it(forward_iterator ins_it, T& value);
-        void insert_by_number(size_t N, T& value);
+
+        void insert_by_it(forward_iterator ins_it, T &value);
+
+        void insert_by_number(size_t N, T &value);
+
         size_t Size();
+
     private:
         using allocator_type = typename Allocator::template rebind<element>::other;
 
         struct deleter {
-            deleter(allocator_type* allocator) : allocator_(allocator) {}
+            deleter(allocator_type *allocator) : allocator_(allocator) {}
 
-            void operator() (element* ptr) {
+            void operator()(element *ptr) {
                 if (ptr != nullptr) {
                     std::allocator_traits<allocator_type>::destroy(*allocator_, ptr);
                     allocator_->deallocate(ptr, 1);
@@ -57,31 +74,34 @@ namespace containers {
             }
 
         private:
-            allocator_type* allocator_;
+            allocator_type *allocator_;
         };
 
         struct element {
             T value;
-            std::unique_ptr<element, deleter> next_element{ nullptr, deleter{nullptr} };
-            element(const T& value_) : value(value_) {}
+            std::unique_ptr<element, deleter> next_element{nullptr, deleter{nullptr}};
+
+            element(const T &value_) : value(value_) {}
+
             forward_iterator next();
         };
-        static std::unique_ptr<element> push_impl(std::unique_ptr<element> cur, const T& value);
+
+        static std::unique_ptr<element> push_impl(std::unique_ptr<element> cur, const T &value);
+
         //std::unique_ptr<element> first = nullptr;
         allocator_type allocator_{};
-        std::unique_ptr<element, deleter> first{ nullptr, deleter{nullptr} };
+        std::unique_ptr<element, deleter> first{nullptr, deleter{nullptr}};
     };
 
     template<class T, class Allocator>
-    void queue<T, Allocator>::push(const T& value) {
-        element* tmp = this->allocator_.allocate(1);
+    void queue<T, Allocator>::push(const T &value) {
+        element *tmp = this->allocator_.allocate(1);
         std::allocator_traits<allocator_type>::construct(this->allocator_, tmp, value);
         if (first == nullptr) {
-            first = std::unique_ptr<element, deleter>(tmp, deleter{ &this->allocator_ });
-        }
-        else {
+            first = std::unique_ptr<element, deleter>(tmp, deleter{&this->allocator_});
+        } else {
             std::swap(tmp->next_element, first);
-            first = std::move(std::unique_ptr<element, deleter>(tmp, deleter{ &this->allocator_ }));
+            first = std::move(std::unique_ptr<element, deleter>(tmp, deleter{&this->allocator_}));
         }
         size++;
     }
@@ -96,16 +116,16 @@ namespace containers {
         return forward_iterator(nullptr);
     }
 
-
     template<class T, class Allocator>
     void queue<T, Allocator>::pop() {
         if (size == 0) {
             throw std::logic_error("queue is empty");
         }
-    }
+        this->first = std::move(this->begin().it_ptr->next_element);
+    mrand48()}
 
     template<class T, class Allocator>
-    T& queue<T, Allocator>::top() {
+    T &queue<T, Allocator>::top() {
         if (size == 0) {
             throw std::logic_error("stack is empty");
         }
@@ -120,12 +140,13 @@ namespace containers {
     }
 
     template<class T, class Allocator>
-    std::unique_ptr<typename queue<T, Allocator>::element> queue<T, Allocator>::push_impl(std::unique_ptr<element> cur, const T& value) {
+    std::unique_ptr<typename queue<T, Allocator>::element>
+    queue<T, Allocator>::push_impl(std::unique_ptr<element> cur, const T &value) {
         if (cur != nullptr) {
             cur->next_element = push_impl(std::move(cur->next_element), value);
             return cur;
         }
-        return std::unique_ptr<element>(new element{ value });
+        return std::unique_ptr<element>(new element{value});
     }
 
     template<class T, class Allocator>
@@ -147,21 +168,18 @@ namespace containers {
     template<class T, class Allocator>
     void queue<T, Allocator>::delete_by_number(size_t N) {
         forward_iterator it = this->begin();
-        for (size_t i = 0; i < N+1; ++i) {
-            if (i == N) break;
-            ++it;
-        }
+        for (size_t i = 0; i < N; ++i, ++it) {}
         this->delete_by_it(it);
     }
 
     template<class T, class Allocator>
-    void queue<T, Allocator>::insert_by_it(containers::queue<T, Allocator>::forward_iterator ins_it, T& value) {
-        element* tmp = this->allocator_.allocate(1);
+    void queue<T, Allocator>::insert_by_it(containers::queue<T, Allocator>::forward_iterator ins_it, T &value) {
+        element *tmp = this->allocator_.allocate(1);
         std::allocator_traits<allocator_type>::construct(this->allocator_, tmp, value);
         forward_iterator i = this->begin();
         if (ins_it == this->begin()) {
             tmp->next_element = std::move(first);
-            first = std::move(std::unique_ptr<element, deleter>(tmp, deleter{ &this->allocator_ }));
+            first = std::move(std::unique_ptr<element, deleter>(tmp, deleter{&this->allocator_}));
             size++;
             return;
         }
@@ -170,14 +188,14 @@ namespace containers {
         }
         if (i.it_ptr == nullptr) throw std::logic_error("out of borders");
         tmp->next_element = std::move(i.it_ptr->next_element);
-        i.it_ptr->next_element = std::move(std::unique_ptr<element, deleter>(tmp, deleter{ &this->allocator_ }));
+        i.it_ptr->next_element = std::move(std::unique_ptr<element, deleter>(tmp, deleter{&this->allocator_}));
         size++;
     }
+
     template<class T, class Allocator>
-    void queue<T, Allocator>::insert_by_number(size_t N, T& value) {
+    void queue<T, Allocator>::insert_by_number(size_t N, T &value) {
         forward_iterator it = this->begin();
-        for (size_t i = 0; i < N+1; ++i) {
-            if (i == N) break;
+        for (size_t i = 0; i < N; ++i) {
             ++it;
         }
         this->insert_by_it(it, value);
@@ -189,17 +207,17 @@ namespace containers {
     }
 
     template<class T, class Allocator>
-    queue<T, Allocator>::forward_iterator::forward_iterator(containers::queue<T, Allocator>::element* ptr) {
+    queue<T, Allocator>::forward_iterator::forward_iterator(containers::queue<T, Allocator>::element *ptr) {
         it_ptr = ptr;
     }
 
     template<class T, class Allocator>
-    T& queue<T, Allocator>::forward_iterator::operator*() {
+    T &queue<T, Allocator>::forward_iterator::operator*() {
         return this->it_ptr->value;
     }
 
     template<class T, class Allocator>
-    typename queue<T, Allocator>::forward_iterator& queue<T, Allocator>::forward_iterator::operator++() {
+    typename queue<T, Allocator>::forward_iterator &queue<T, Allocator>::forward_iterator::operator++() {
         if (it_ptr == nullptr) throw std::logic_error("out of queue borders");
         *this = it_ptr->next();
         return *this;
@@ -208,17 +226,17 @@ namespace containers {
     template<class T, class Allocator>
     typename queue<T, Allocator>::forward_iterator queue<T, Allocator>::forward_iterator::operator++(int) {
         forward_iterator old = *this;
-        ++* this;
+        ++*this;
         return old;
     }
 
     template<class T, class Allocator>
-    bool queue<T, Allocator>::forward_iterator::operator==(const forward_iterator& other) const {
+    bool queue<T, Allocator>::forward_iterator::operator==(const forward_iterator &other) const {
         return it_ptr == other.it_ptr;
     }
 
     template<class T, class Allocator>
-    bool queue<T, Allocator>::forward_iterator::operator!=(const forward_iterator& other) const {
+    bool queue<T, Allocator>::forward_iterator::operator!=(const forward_iterator &other) const {
         return it_ptr != other.it_ptr;
     }
 }
